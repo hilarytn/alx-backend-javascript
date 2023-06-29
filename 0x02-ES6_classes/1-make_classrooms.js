644cab43a7f7faf4833557d4490eae2d16eb5c83
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2023-06-30 00:56:42 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,9 @@
-import ClassRoom from './0-classroom.js';
-
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const arr = [];
-
-  const class19 = new ClassRoom(19);
-  arr.push(class19);
-
-  const class20 = new ClassRoom(20);
-  arr.push(class20);
-
-  const class34 = new ClassRoom(34);
-  arr.push(class34);
-
-  return arr;
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return (newList);
 }
