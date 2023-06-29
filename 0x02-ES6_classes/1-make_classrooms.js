a9cd98c806c2447c517e31642c8ae87efa23ab81
Hilary Titus Naor <hilarytn@gmail.com>
2023-06-29 23:29:36 +0000

    Refactor code and add extra line beefore class definition

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,16 +1,12 @@
 import ClassRoom from './0-classroom.js';
 
+
 export default function initializeRooms() {
   const arr = [];
 
   const class19 = new ClassRoom(19);
-  arr.push(class19);
-
   const class20 = new ClassRoom(20);
-  arr.push(class20);
-
   const class34 = new ClassRoom(34);
-  arr.push(class34);
 
-  return arr;
+  return arr[class19, class20, class34];
 }
