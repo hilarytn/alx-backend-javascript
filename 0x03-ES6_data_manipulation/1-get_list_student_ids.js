2a9c683171b3f46c8d0563555d6a6e9d200855f6
Hilary Titus Naor <hilarytn@gmail.com>
2023-07-04 22:26:45 +0000

    get student by id

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,6 @@
-export default function getStudentsByLocation(list, city) {
-  return list.filter((obj) => obj.location === city);
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
+  }
+  return [];
 }
