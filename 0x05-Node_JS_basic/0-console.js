e246e1f35c3b89d553f56194a81a17e883abe4e9
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-22 13:34:07 +0000

    Fix argument to camel case

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(string_to_display) {
-  console.log(string_to_display);
+// 0-console.js
+/**
+ * Prints text to STDOUT
+ * @stringToDisplay - tesxt to display
+ */
+function displayMessage(stringToDisplay) {
+  console.log(stringToDisplay);
 }
 
 module.exports = displayMessage;
