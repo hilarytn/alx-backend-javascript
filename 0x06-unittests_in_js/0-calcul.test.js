7dc302bbede40e5a2180c289783447e0b1feff68
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-24 18:24:23 +0000

    Add more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -11,7 +11,7 @@ describe('calculateNumber', () => {
   it('should return the sum of two decimal as a whole number rounded up', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return the sum of two decimal numbers with parts greater than 5 as a whole number rounded up', () => {
+  it('should return the sum of two decimal numbers as a whole number', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
   it('should return the sum of two zero decimal numbers', () => {
@@ -19,5 +19,36 @@ describe('calculateNumber', () => {
   });
   it('should return the sum of 0 as 0', () => {
     assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 4);
+  });
+
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 4);
+  });
+
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 4);
+  });
+
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 6);
   });
 });
