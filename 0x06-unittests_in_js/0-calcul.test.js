e620e02406112e3c47e80a4f18a7b168ffab0504
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-24 17:46:24 +0000

    Revert to older tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,20 @@
-const calculateNumber = require("./0-calcul.js");
 const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('rounding of a', () => {
-        assert.equal(calculateNumber(15.78, 2), 18);
-        assert.equal(calculateNumber(1.2, 0), 1);
-        assert.equal(calculateNumber(3.5, 2), 6);
-    });
+  it('should return the sum of two whole numbers', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
 
-    it('rounding of b', () => {
-        assert.equal(calculateNumber(2, 15.78), 18);
-        assert.equal(calculateNumber(0, 1.2), 1);
-        assert.equal(calculateNumber(2, 3.5), 6);
-    });
+  it('should return the sum of whole number and a decimal as a whole number rounded up', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
 
-    it('suming of a and b', () => {
-        assert.equal(calculateNumber(15.78, 2.5), 19);
-        assert.equal(calculateNumber(1.2, 0.2), 1);
-        assert.equal(calculateNumber(3.5, 2.7), 7);
-    });
-})
+  it('should return the sum of two decimal as a whole number rounded up', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return the sum of two decimal numbers with parts greater than 5 as a whole number rounded up', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
