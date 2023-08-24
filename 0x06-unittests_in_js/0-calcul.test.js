308be79a48de8eb30bc40ed24b7322876ea4b53a
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-24 17:58:24 +0000

    Add more tests cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,16 +5,19 @@ describe('calculateNumber', () => {
   it('should return the sum of two whole numbers', () => {
     assert.equal(calculateNumber(1, 3), 4);
   });
-
   it('should return the sum of whole number and a decimal as a whole number rounded up', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-
   it('should return the sum of two decimal as a whole number rounded up', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-
   it('should return the sum of two decimal numbers with parts greater than 5 as a whole number rounded up', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return the sum of two zero decimal numbers', () => {
+    assert.equal(calculateNumber(1.0, 2.0), 3);
+  });
+  it('should return the sum of 0 as 0', () => {
+    assert.equal(calculateNumber(0, 0), 0);
   });
 });
