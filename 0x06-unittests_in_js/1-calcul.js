bc8d28cbf4ee9469df4b14aa2bb6ba78894cd740
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-24 21:43:34 +0000

    Add type argument to the calculation function

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,17 @@
+function calculateNumber(type, a, b) {
+  const val1 = Math.round(a);
+  const val2 = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return val1 + val2;
+    case 'SUBTRACT':
+      return val1 - val2;
+    case 'DIVIDE':
+      if (val2 === 0) return 'Error';
+      return val1 / val2;
+    default:
+  }
+  return 0;
+}
+module.export = calculateNumber;
