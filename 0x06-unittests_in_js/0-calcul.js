9ead1f381a22ffb339df48061cef7e834e9f780f
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-24 17:24:41 +0000

    Run Mocha tests for calculation

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+function calculateNumber(a, b) {
+  return (Math.ceil(a + b));
+}
+// export default calculateNumber;
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should return the sum of two whole numbers', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+
+  it('should return the sum of whole number and a decimal as a whole number rounded up', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return the sum of two decimal as a whole number rounded up', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return the sum of two decimal as a whole number rounded up', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
